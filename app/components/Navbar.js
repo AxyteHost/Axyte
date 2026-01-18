36bcb61b9d4c34b4a7a38aeafc29723d80fd97e2
Eiro.tf <freakgaming2009@gmail.com>
2026-01-18 13:07:24 +0530

    Update Navbar.js

--- app/components/Navbar.js
+++ app/components/Navbar.js
@@ -46,8 +46,8 @@ const Navbar = () => {
       >
         <div className="max-w-[1600px] mx-auto flex items-center justify-between h-16 px-8">
           {/* Logo */}
-          <div className="flex items-center space-x-19">
-            <div className="flex items-center space-x-20">
+          <div className="flex items-center space-x-29">
+            <div className="flex items-center space-x-30">
               <div className="relative w-8 h-8">
                 <Image
                   src="img/logo.png"
